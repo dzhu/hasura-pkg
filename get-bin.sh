82afe2bb246d1d8bf458c0773a24c83516ebf44c
Danny Zhu <dzhu@dzhu.us>
2020-02-24 12:15:24 -0800

    Switch to using a wrapper script for reading config
    `EnvironmentFile` is nice, but this means that the configuration file is
    considered even when running outside systemd.

--- get-bin.sh
+++ get-bin.sh
@@ -3,5 +3,5 @@ version="${1:-1.0.0}"
 mkdir -p bin
 
 img_id=$(docker create hasura/graphql-engine:v"$version")
-docker cp "$img_id":/bin/graphql-engine bin/hasura-"$version"
+docker cp "$img_id":/bin/graphql-engine bin/graphql-engine-"$version"
 docker rm "$img_id" > /dev/null
--- make-pkg.sh
+++ make-pkg.sh
@@ -7,7 +7,7 @@ version="${1:-1.0.0}"
 # Create an "upstream tarball" in a fresh directory.
 rm -rf build
 mkdir build
-tar czf build/hasura-"$version".tar.gz --xform "s|.*|hasura-$version/hasura|" bin/hasura-"$version"
+tar czf build/hasura-"$version".tar.gz --xform "s|.*|hasura-$version/graphql-engine|" bin/graphql-engine-"$version"
 cd build
 
 # Now work with the tarball normally.
